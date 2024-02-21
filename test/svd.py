f86804c501cefb67650d9a5f2527a45487700ea3
jamxiao2025 <95207937+jamxiao2025@users.noreply.github.com>
2024-02-21 17:25:02 -0500

    revert to original file state

--- test/svd.py
+++ test/svd.py
@@ -3,7 +3,6 @@ import sys
 import numpy as np
 from PIL import Image
 import time
-
 def calculate_file_size(filepath):
     return os.path.getsize(filepath)
 
@@ -13,13 +12,22 @@ def compress_image(input_filename, output_folder, k):
 
 	image = Image.open(input_image_path)
 	image_data = np.array(image)
+
 	print("Bytes required for original image: " + str(image_data.nbytes))
 	# Separate color channels
 	channels = [image_data[:, :, i] for i in range(image_data.shape[2])]
 	compressed_image_data = np.zeros((image_data.shape[0],image_data.shape[1], image_data.shape[2]))
 	for i,channel in enumerate(channels):
+		svd_start_time = time.time()
 		channel_u, channel_s, channel_v = np.linalg.svd(channel, full_matrices=False)
+		print(type(channel_u))
+		svd_end_time = time.time()
+		print(f"SVD computation time: {svd_end_time - svd_start_time}")
+		mat_mul_time_start = time.time()
+		print("Hello, it will crash on the next line")
 		k_rank_approx = channel_u[:, 0:k].dot(np.diag(channel_s[0:k]).dot(channel_v[0:k,:]))
+		mat_mul_time_end = time.time()
+		print(f"Matrix multiplication time: {mat_mul_time_end - mat_mul_time_start}")
 		print("Bytes required for compressed image: " + str(channel_u[:,0:k].nbytes + np.diag(channel_s[0:k]).nbytes + channel_v[0:k,:].nbytes))
 		compressed_image_data[:,:,i] = k_rank_approx
 	# Save compressed image
@@ -34,7 +42,6 @@ def compress_image(input_filename, output_folder, k):
 	print(f"Compressed File Size (Rank {k}): {compressed_file_size} bytes")
 	percent_compression = (1 - (compressed_file_size / original_file_size)) * 100
 	print(f"Percent Compression (Rank {k}): {percent_compression}%")
-
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
 		print("Usage: python svd.py <k>")
